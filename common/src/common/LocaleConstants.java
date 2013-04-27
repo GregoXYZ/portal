2e3db1558a8eac55b906e7af72acf08c79315bd4
Grego <GregoXYZ@hotmail.com>
2013-04-27 23:04:53 +0200

    Locales

--- common/src/common/LocaleConstants.java
+++ common/src/common/LocaleConstants.java
@@ -4,6 +4,5 @@ public interface LocaleConstants {
     public static final String LANGUAGE_SPANISH = "es";
     public static final String LANGUAGE_CATALAN = "ca";
     public static final String LANGUAGE_ENGLISH = "en";
-
     public static final String DEFAULT_LOCALE = LocaleConstants.LANGUAGE_SPANISH;
 }
